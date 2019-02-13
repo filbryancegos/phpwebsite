fcbeee858f59249114851531975a3669a65fa99b
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2019-02-13 20:44:32 +0800

    added simple input validations

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 var headerInfo = document.querySelectorAll('.header-info');
 var headerInfoList = Array.from(headerInfo);
 
-headerInfoList.forEach(function(elem) { 
-	
-	var dotsList = elem.querySelectorAll('.dots-indicators li');
-	var dots = Array.from(dotsList); 
+headerInfoList.forEach(function(elem) {
 
-	dots.forEach(function(dot) { dot.addEventListener('click', handleclick); })	
+	var dotsList = elem.querySelectorAll('.dots-indicators li');
+	var dots = Array.from(dotsList);
+
+	dots.forEach(function(dot) { dot.addEventListener('click', handleclick); })
 
 	function handleclick(e) {
-		
+
 		var parent = e.target.parentNode;
 		var tooltip = parent.querySelector('.info-basic-tooltip');
-		console.log(tooltip)	
-		
+		console.log(tooltip)
+
 		toggle(tooltip);
 	}
 
@@ -29,4 +29,44 @@ headerInfoList.forEach(function(elem) {
 	// Otherwise, show it
 	show(elem);
 };
+})
+
+
+
+ var form = document.querySelector('#form-sign-2');
+ var submit = document.querySelector('#submitform');
+
+function showAlert(message, className) {
+	var div = document.createElement('div');
+	div.className = 'alert' + " " + 'alert-'+className;
+	div.appendChild(document.createTextNode(message));
+	var container = document.querySelector('.sign-wrapper');
+	var form = document.querySelector('#form-sign-in');
+	container.insertBefore(div, form);
+
+	setTimeout(function() {
+		document.querySelector('.alert').remove()
+	}, 3000)
+}
+
+function clearFields() {
+	document.querySelector('#email').value = '';
+	document.querySelector('#password').value = '';
+	document.querySelector('#confirmpassword').value = '';
+ }
+
+submit.addEventListener('click', function(e) {
+	e.preventDefault();
+	// Get form values
+	var email = document.querySelector('#email').value;
+	var pass = document.querySelector('#password').value;
+	var confirmpass = document.querySelector('#confirmpassword').value;
+	
+	// Validate
+    if(email === '' || pass === '' || confirmpass === '') {
+	  showAlert('Please fill in all fields', 'danger');
+    } else {
+		//form.submit();
+		clearFields();
+    }
 })
